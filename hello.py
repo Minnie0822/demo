c5f418f93733241dc201a51999990afb7d314565
busisiwe phindiso <42931860+Minnie0822@users.noreply.github.com>
2024-10-02 15:18:27 +0200

    hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = (input ("How old are you? "))
+
+location = input("where do you live? ")
+
+print("Hello")
+print(f" You are {age} ")
+print("live in Rondebult")
+
